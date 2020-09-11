9318438382bc21d39e43a1eec0fad3ebc6fce96d
Ihor Katkov <ihorkatkov@gmail.com>
2020-09-12 01:00:04 +0300

    Add nearest tasks endpoint

--- lib/geo_tracker_web/router.ex
+++ lib/geo_tracker_web/router.ex
@@ -12,10 +12,21 @@ defmodule GeoTrackerWeb.Router do
     plug VerifyApiKey, user_role: "manager"
   end
 
+  pipeline :authed_driver do
+    plug :accepts, ["json"]
+    plug VerifyApiKey, user_role: "driver"
+  end
+
   scope "/api/tasks", GeoTrackerWeb do
     pipe_through :authed_manager
 
     post "/tasks", TaskController, :create
+  end
+
+  scope "/api/tasks", GeoTrackerWeb do
+    pipe_through :authed_driver
+
+    get "/tasks", TaskController, :new_nearest
   end
 
   scope "/api", GeoTrackerWeb do
